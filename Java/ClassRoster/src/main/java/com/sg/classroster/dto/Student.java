c52ebeadcc98fecf5e416a4936c9bf4d93e9b0d2
tingm3 <tingyue.dong@mthree.com>
2026-03-08 17:57:00 -0400

    Delete Java directory

--- Java/ClassRoster/src/main/java/com/sg/classroster/dto/Student.java
+++
@@ -1,6 +0,0 @@
-package com.sg.classroster.dto;
-
-import java.util.Objects;
-
-public record Student(String firstName, String lastName, String studentId, String cohort) {
-}
--- Java/ClassRoster/src/test/java/com/sg/classroster/dao/ClassRosterAuditDaoStubImpl.java
+++
@@ -1,9 +0,0 @@
-package com.sg.classroster.dao;
-
-public class ClassRosterAuditDaoStubImpl implements  ClassRosterAuditDao{
-    @Override
-    public void writeAuditEntry(String entry) throws ClassRosterPersistenceException {
-        //do nothing . . .
-    }
-}
-
